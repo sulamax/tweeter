407e48c3c15d87c6124a63b85a907b80ff52128a
sulamax <saraujo0135@gmail.com>
2019-06-12 17:12:04 -0400

    saving css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,60 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+$(document).ready(function() {
+function renderTweets(tweets) {
+
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+}
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+$(document).ready(function() {
+  function createTweetElement (tweetData) {
+    // console.log(tweetData);
+    var $template = `
+      <section class="tweet-container">
+          <header>
+            <img class="smile" src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png">
+            <h1>bill Fields</h1>
+            <p>@MrFields</p>
+            </header>
+            <article>
+              <h4>Little tweet here</h4>
+            </article>
+            <footer>
+              10 days ago
+              <img class="image" src="https://img.icons8.com/windows/24/000000/like.png">
+              <img class="image" src="https://img.icons8.com/metro/23/000000/process.png">
+              <img class="image" src="https://img.icons8.com/metro/28/000000/remove-user-male.png">
+              </main>
+            </footer>
+        </section>
+    `
+      return $template
+    console.log($template);
+  }
+var $tweet = createTweetElement(tweetData);
+console.log($tweet);
+$('.all-tweets').append($tweet);
+createTweetElement(tweetData);
+});
+
+renderTweets(data);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,40 @@
 $(document).ready(function() {
   console.log(this);
 
-$("textarea").on("click",function(e) {
-  console.log("click", e);
-});
+  var input = "";
 
-$("textarea").on("keyup",function(e) {
-  console.log("keyup", e);
-});
+  $("textarea").on("keyup",function(e) {
+    input += e.key;
+    console.log("keyup", input);
+  });
 
-$("#btn").on('click', function() {
-  console.log(this); //The this keyword is a reference to the button
-});
+  $("#btn").on('click', function(e) {
+    e.preventDefault();
+    console.log("click", $(this).siblings(".name"));
+  });
 
 function updateCountdown() {
-  var remaining = 140 - jQuery('.message').val().length;
-  jQuery('.counter').text(remaining);
+  var remaining = 140 - $('.message').val().length;
+  $('.countdown').text(remaining);
 }
 
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
   updateCountdown();
   $('.message').change(updateCountdown);
   $('.message').keyup(updateCountdown);
 });
+
+$("#mytextarea").keyup(function() {
+var maxlength = 140;
+if($("#mytextarea").val().length > maxlength) {
+$("#mytextarea").css('color','red');
+}
+else{
+$("#mytextarea").css('color','black');
+}
 });
+});
+
+
 
 
